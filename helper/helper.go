8adf055b503d720999ab351ffaf0c856830cde48
zandala88 <lkx13662254197@163.com>
2023-12-28 09:41:39 +0800

    update httpRequest

--- helper/helper.go
+++ helper/helper.go
@@ -1,12 +1,16 @@
 package helper
 
 import (
+	"bytes"
 	"crypto/md5"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"gin_admin/define"
 	"github.com/dgrijalva/jwt-go"
 	uuid "github.com/satori/go.uuid"
+	"io"
+	"net/http"
 	"strings"
 )
 
@@ -61,4 +65,59 @@ func RFC3339ToNormalTime(rfc3339 string) string {
 
 func UUID() string {
 	return uuid.NewV4().String()
+}
+
+// httpRequest .
+func httpRequest(url, method string, data, header []byte) ([]byte, error) {
+	var err error
+	reader := bytes.NewBuffer(data)
+	request, err := http.NewRequest(method, url, reader)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Set("Content-Type", "application/json;charset=UTF-8")
+	// 处理 header
+	if len(header) > 0 {
+		headerMap := new(map[string]interface{})
+		err = json.Unmarshal(header, headerMap)
+		if err != nil {
+			return nil, err
+		}
+		for k, v := range *headerMap {
+			if k == "" || v == "" {
+				continue
+			}
+			request.Header.Set(k, v.(string))
+		}
+	}
+
+	client := http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	respBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return respBytes, nil
+}
+
+func HttpDelete(url string, data []byte, header ...byte) ([]byte, error) {
+	return httpRequest(url, "DELETE", data, header)
+}
+
+func HttpPut(url string, data []byte, header ...byte) ([]byte, error) {
+	return httpRequest(url, "PUT", data, header)
+}
+
+func HttpPost(url string, data []byte, header ...byte) ([]byte, error) {
+	return httpRequest(url, "POST", data, header)
+}
+
+func HttpGet(url string, header ...byte) ([]byte, error) {
+	return httpRequest(url, "GET", []byte{}, header)
 }
